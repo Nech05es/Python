59febbf09d38f71914d8f21d06b8427b97430f1f
Nech05es <96850480+Nech05es@users.noreply.github.com>
2026-07-15 21:19:04 -0700

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,48 @@
+# Variables #
+'''
+Que es una Variable ?
+
+Una variable en programacion es un espacio en memoria (ram) donde se almacena un tipo de dato,
+el cual se accede usando un nombre el cual especificamos al declarar una variable. El mismo 
+nombre de Variable nos indica que este dato alamcenado puede variar o en terminos sencillos
+en Python no es necesario declarar que tipo de dato almacenara esa variable.
+
+Como se declara una Variable ?
+
+En Python se usa la siguiente estructura : (Nombre > Operador de Asignacion > Valor)
+
+Buenas practicas al declara variables:
+
+(1) Al nombrar una variable esta tiene que ser en minusculas para una variable que puede cambiar
+de valor o tipo de dato, este tampoco puede iniciar con signos ni numeros.
+(2) Para asignar un valor a la variable usamos el Operador de Asignacion siendo el signo aritmetico
+de igualdad (=).
+
+Que tipos de Datos puede almacenar una Variable?
+
+Son 5 tipos de datos que una variable puede almacenar en este lenguaje de programacion.
+
+Tipo Numerico: Estos son Numeros Enteros (10,305,5000)
+Tipo Flotante: Estos son Numeros Decimales (2.5,32.50,5000.1)
+Tipo String: Estos son Cadenas de Caracteres ("Hola","Ser Humano","Bonito")
+Tipo Boolean: Estos son Datos tipo Binario representados con (True,False)
+Tipo None: Este es un Dato vacio representado por (None)
+
+Ahora que ya sabes que es una variable intenta crear algunas y usar la funcion (print) que
+ofrece python para poder imprimir en una consola algun tipo de dato.
+'''
+
+numero = 10
+print(numero) # Resultado --> 10
+
+flotante = 3.1416
+print(flotante) # Resultado --> 3.1416
+
+string = "Mi primer Hola en python"
+print(string) # Resultado --> Mi primer Hola en python
+
+boolean = False
+print(boolean) # Resultado --> False
+
+none = None
+print(none) # Resultado --> None
